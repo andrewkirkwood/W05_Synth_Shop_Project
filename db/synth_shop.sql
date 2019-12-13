b42ca22b50da1567f86a673192429b3219379be6
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-13 14:43:07 +0000

    setting up SQL

---
+++ db/synth_shop.sql
@@ -0,0 +1,20 @@
+DROP TABLE products;
+DROP TABLE manufacturers;
+
+CREATE TABLE manufacturers
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+);
+
+CREATE TABLE products
+(
+  id SERIAL primary key,
+  name VARCHAR(255) not null,
+  description VARCHAR(255),
+  buy_cost INT,
+  sell_price INT,
+  stock_qty INT,
+  sales_qty INT
+  manufacturer_id INT references manufacturers(id)
+);
